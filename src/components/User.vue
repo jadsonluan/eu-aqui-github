e5abf6a54b790f598b3ab2075f73cdb1a576618c
Jadson Luan <jadsonluan1@gmail.com>
2019-04-19 13:52:36 -0300

    Implementa log out

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="user-card">
-    <img :src="avatar_url" />
-    <h1>Jadson Luan</h1>
-    <a href="https://github.com/jadsonluan">@jadsonluan</a>
+  <div id="user">
+    <div class="user-card">
+      <img :src="avatar_url" />
+      <h1>Jadson Luan</h1>
+      <a href="https://github.com/jadsonluan">@jadsonluan</a>
+    </div>
+    <div>
+      <button v-on:click='onLogout' class="logout-btn">Log out</button>
+    </div>
   </div>
 </template>
 
@@ -13,11 +18,22 @@ export default {
     return {
       avatar_url: require('../assets/temp-avatar.jpeg'),
     }
+  },
+  methods: {
+    onLogout() {
+      this.$cookie.delete("token");
+      this.$router.go();
+    }
   }
 }
 </script>
 
 <style>
+#user {
+  display: grid;
+  grid-template-columns: min-content 2fr;
+  grid-gap: 50px;
+}
 
 div.user-card {
   width: 300px;
@@ -30,20 +46,34 @@ div.user-card img {
   height: 300px;
 }
 
-h1 { margin-bottom: 2px; }
+#user h1 { margin-bottom: 2px; }
 
-a { 
-  /* #84CF6A */
-  /* #16C0B0 */
+#user a { 
   color: #16C0B0; 
   text-decoration: none; 
   font-weight: bold; 
   font-size: 18px;
 }
 
-a:hover {
+#user a:hover {
   color: #84CF6A;
   transition-duration: 0.33s;
+}
+
+#user .logout-btn {
+  float: right;
+  background: #2c3e50;
+  color: white;
+  border-radius: 30px;
+  padding: 5px 15px;
+  border: 2px solid #2c3e50;
+  cursor: pointer;
+}
+
+#user .logout-btn:hover {
+  transition-duration: 0.5s;
+  background-color: white;
+  color: #2c3e50;
 }
 
 </style>
